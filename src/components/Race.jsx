ce5f985e5692e90984a41c66a6f112491f868d45
Bob Smith <wasadmin@webagesolutions.com>
2022-10-19 16:05:29 -0400

    Added contestants to state, move contestants across screen

--- src/components/Race.jsx
+++ src/components/Race.jsx
@@ -1,15 +1,35 @@
 import Contestant from './Contestant.jsx';
 import { contestantStyles } from '../styles';
 import { contestants } from '../assets/data';
+import {useState, useEffect} from 'react';
 
-function Race() {
+function Race({currentTime}) {
+
+  useEffect(() => {
+    // console.log(allContestants);
+    // console.log(currentTime);
+    move();
+  }, [currentTime]);
+
+  const [allContestants, setAllContestants] = useState(contestants);
+
+  const move = () => {
+    for(let i = 0; i < allContestants.length; i++) {
+      const interval = Math.floor(Math.random()*10);
+      const newXpos = allContestants[i].xpos + interval;
+      allContestants[i].xpos = newXpos;
+      setAllContestants([...allContestants]);
+      // console.log(allContestants[i].xpos);
+    }
+  };
+
   return (
     <div style={contestantStyles.contestantList}>
-      {contestants.map((contestant) => (
+      {allContestants.map((person) => (
         <Contestant
-          key={contestant.id}
-          name={contestant.name}
-          xpos={contestant.xpos}
+          key={person.id}
+          name={person.name}
+          xpos={person.xpos}
         />
       ))}
     </div>
